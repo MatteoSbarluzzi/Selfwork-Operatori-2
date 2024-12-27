b0a3fe403f29b32eb2983a8662c03af3de782a1c
MatteoSbarluzzi <matteosbarluzzi1@gmail.com>
2024-12-27 11:09:12 +0100

    Selfwork Operatori 2

---
+++ main2.js
@@ -0,0 +1,8 @@
+const annoCorrente = 2024; 
+const annoNascita = 1984; 
+
+const età = annoCorrente - annoNascita;
+console.log(età);
+
+const anniRaggiungimentoCento = 100 - età;
+console.log(`La persona necessita di ${anniRaggiungimentoCento} anni per arrivare a 100`);
